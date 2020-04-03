ccf2cbcbd35515147152c3efa682c7cb1c94f0b2
vjguzman <vjguzman@miuandes.cl>
2020-04-02 21:06:38 -0300

    borrar app

--- app/models/tickets_order.rb
+++
@@ -1,2 +0,0 @@
-class TicketsOrder < ApplicationRecord
-end
--- db/migrate/20200402220834_create_tickets_orders.rb
+++
@@ -1,8 +0,0 @@
-class CreateTicketsOrders < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tickets_orders do |t|
-
-      t.timestamps
-    end
-  end
-end
